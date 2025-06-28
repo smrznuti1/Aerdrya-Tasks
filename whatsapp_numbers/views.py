57ff73040669adedb2e41f11a5f0b9d46c3a4234
smrznuti1 <filipbozovic998@gmail.com>
2025-06-28 18:04:23 +0200

    Add Number Transformation Script

--- whatsapp_numbers/views.py
+++ whatsapp_numbers/views.py
@@ -2,6 +2,37 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+def __transform_number(number):
+    digits = str(number)
+    return (" ").join([__transform_digit(string_digit) for string_digit in digits])
+
+
+def __transform_digit(digit):
+    match digit:
+        case "0":
+            return "zero"
+        case "1":
+            return "one"
+        case "2":
+            return "two"
+        case "3":
+            return "three"
+        case "4":
+            return "four"
+        case "5":
+            return "five"
+        case "6":
+            return "six"
+        case "7":
+            return "seven"
+        case "8":
+            return "eight"
+        case "9":
+            return "nine"
+
+
 # Create your views here.
 def get_number(request):
-    return HttpResponse("This is a request for WhatsApp Number Change")
+    number = 43424
+    string_number = __transform_number(43424)
+    return HttpResponse(f"This is your number: {string_number}")
