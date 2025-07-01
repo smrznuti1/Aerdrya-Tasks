2316d7f2d4cca4c53c141c150d4b33e7449ae685
smrznuti1 <filipbozovic998@gmail.com>
2025-07-01 19:27:49 +0200

    View database address

--- whatsapp_numbers/urls.py
+++ whatsapp_numbers/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_number, name="get_number"),
+    path("", views.welcome, name="welcome"),
+    path("<int:number>/", views.get_number, name="get_number"),
+    path("database", views.list_database, name="list_database"),
 ]
--- whatsapp_numbers/views.py
+++ whatsapp_numbers/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
 
+from .models import Number
+
 # from django.shortcuts import render
 
 
 # Create your views here.
-def get_number(request):
-    return HttpResponse("This is your number placeholder.")
+def get_number(request, number):
+    return HttpResponse(f"Wow, {number} is a nice number.")
+
+
+def list_database(request):
+    numbers = Number.objects.all()
+    output = ", ".join([str(number) for number in numbers])
+    return HttpResponse(output)
+
+
+def welcome(request):
+    return HttpResponse("Welcome.")
