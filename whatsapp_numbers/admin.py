d5e1f2f0fa3206fcfa500370cecc1efde936cf8e
smrznuti1 <filipbozovic998@gmail.com>
2025-07-01 19:19:25 +0200

    Add Number String model

--- whatsapp_numbers/admin.py
+++ whatsapp_numbers/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Number
+from .models import Number, StringNumber
 
 # Register your models here.
 admin.site.register(Number)
+admin.site.register(StringNumber)
