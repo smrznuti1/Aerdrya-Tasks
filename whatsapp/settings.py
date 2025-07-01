6f133e805ab2ef209d25fd3348ffcbac7e6074e1
smrznuti1 <filipbozovic998@gmail.com>
2025-07-01 13:57:10 +0200

    Add app to installed apps

--- whatsapp/settings.py
+++ whatsapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "whatsapp_numbers.apps.WhatsappNumbersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
